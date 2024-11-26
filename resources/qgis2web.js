6d799221c54931139b8ce640fc62ceac5af82928
SeçGEO_Apps <hamdi.secilmis@gmail.com>
2024-11-26 23:22:12 +0300

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -472,10 +472,7 @@ map.addControl(bottomAttribution);
 
 var attributionList = document.createElement('li');
 attributionList.innerHTML = `
-	<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot;
-	<a href="https://openlayers.org/">OpenLayers</a> &middot;
-	<a href="https://qgis.org/">QGIS</a>	
-`;
+	<a href="https://www.linkedin.com/in/seçgeo-apps-71a48118b/">ⓈeçGEO</a> &middot;	`;
 bottomAttribution.element.appendChild(attributionList);
 
 
