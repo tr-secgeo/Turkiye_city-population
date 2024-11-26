9577ec56399eae9c56adea8357106c002fc6d0c3
SeçGEO_Apps <hamdi.secilmis@gmail.com>
2024-11-26 23:28:28 +0300

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -472,7 +472,7 @@ map.addControl(bottomAttribution);
 
 var attributionList = document.createElement('li');
 attributionList.innerHTML = `
-	<a href="https://www.linkedin.com/in/seçgeo-apps-71a48118b/">ⓈeçGEO</a> &middot;	`;
+	<a href="https://www.linkedin.com/in/seçgeo-apps-71a48118b/">ⓈeçGEO</a>`;
 bottomAttribution.element.appendChild(attributionList);
 
 
